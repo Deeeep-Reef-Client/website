8fd8844b0eba0ec3787d1d5e076e3c4070b3c78d
Seagull <cannedseagull@gmail.com>
2022-10-18 20:42:11 +0800

    check for windows

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 window.addEventListener("load", (event) => {
+    if (!navigator.userAgent.includes("Windows")) alert("Sorry, DRC currently only supports Microsoft Windows");
     if (window.matchMedia("(pointer: coarse)").matches) alert("Sorry, DRC currently does not support mobile devices");
 });
